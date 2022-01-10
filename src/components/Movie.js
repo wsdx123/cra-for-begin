9e897b031bb4d3db8eca0fbe3426b0b4343f3041
wsdx123 <rhftl123@gmail.com>
2022-01-10 22:09:33 +0900

    detail add

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,18 +4,22 @@ import styles from "./Movie.module.css";
 
 const Movie = ({ id, year, coverImg, title, summary, genres }) => {
     return (
-        <div>
-          <img src={coverImg} alt={title} className={styles.movie_img} />
-          <h2 className={styles.movie_title}>
-            <Link to={`/movie/${id}`}>{title}</Link>
-          </h2>
-          <h3 className={styles.movie_year}>{year}</h3>
-          <p>{summary.length > 236 ? `${summary.slice(0, 235)}...` : summary}</p>
-          <ul className={styles.movie_genres}>
-            {genres && genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
+        <div className={styles.container}>
+          <div className={styles.item}>
+            <img src={coverImg} alt={title} className={styles.movie_img} />
+          </div>
+          <div className={styles.item}>
+            <h2 className={styles.movie_title}>
+              <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
+            <h3 className={styles.movie_year}>{year}</h3>
+            <p>{summary.length > 236 ? `${summary.slice(0, 235)}...` : summary}</p>
+            <ul className={styles.movie_genres}>
+              {genres && genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
         </div>
     );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,4 @@
+import { Rate } from "antd";
 import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
@@ -6,11 +7,14 @@ import styles from "./Detail.module.css";
 const Detail = () => {
     const {id} = useParams();
     const [load, setLoad] = useState(true);
+    const [detail,setDetail] = useState([]);
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
         setLoad(false);
+        setDetail(json.data.movie);
+        console.log(json.data.movie);
     };
     useEffect(() => {
         getMovie();
@@ -20,8 +24,27 @@ const Detail = () => {
             {load ? (
                 <Loading />
             ) : (
-                <div className={styles.container}>
-                    good
+                <div>
+                    <div className={styles.backImg} style={{backgroundImage: `url(${detail.large_cover_image})`}} />
+                    <div className={styles.container}>
+                        <div className={styles.item}>
+                            <img src={detail.medium_cover_image} />
+                        </div>
+                        <div className={styles.item}>
+                            <h1>{detail.title_long}</h1>
+                            <ul>
+                                <li>{`Rating ${detail.rating}`}</li>
+                                <li>{`Runtime ${detail.runtime}`}</li>
+                                <li>{`Download ${detail.download_count}`}</li>
+                                <li>
+                                    genre
+                                    <ul>
+                                        {detail.genres && detail.genres.map((x,index) => (<li key={index}>{x}</li>))}
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
